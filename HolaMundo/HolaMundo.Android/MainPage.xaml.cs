08408c5e6eba6529ef7aae34f57258258d8969d6
sebaleoperez <39063689+sebaleoperez@users.noreply.github.com>
2019-11-12 11:45:03 -0300

    Uplodad Hola Mundo !
    First Project

---
+++ HolaMundo/HolaMundo.Android/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace HolaMundo
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        public void ValidarUsuario(object sender, EventArgs args)
+        {
+            if (EntryUsuario.Text == "usuario" &&
+                EntryClave.Text == "12345")
+			{
+				DisplayAlert("Validacion","Usuario Correcto","OK");
+			}
+            else
+			{
+				DisplayAlert("Validacion", "ERROR !", "OK");
+			}
+        }
+    }
+}
---
+++ HolaMundo/HolaMundo/LoginPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace HolaMundo
+{
+    public partial class LoginPage : ContentPage
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+        }
+
+        public void ValidarUsuario(object sender, EventArgs args)
+        {
+            if (EntryUsuario.Text == "usuario" && EntryClave.Text == "12345")
+            {
+                DisplayAlert("Validacion", "Ok !", "OK");
+            }
+            else
+            {
+                DisplayAlert("Validacion", "ERROR !", "OK");
+            }
+        }
+    }
+}
