7cc28ee09f1b80bf994d609ed3e458058c55514d
sebaleoperez <39063689+sebaleoperez@users.noreply.github.com>
2019-11-14 11:40:59 -0300

    Add files via upload
    Clase 2 uploaded

---
+++ HolaMundo/ListadoNumeros.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace HolaMundo
+{
+    public partial class ListadoNumeros : ContentPage
+    {
+        public ListadoNumeros()
+        {
+            InitializeComponent();
+        }
+
+        public void CrearLabels(object sender, EventArgs args)
+        {
+            StackLayoutListadoNumeros.Children.Clear();
+
+            int cantidad = 0;
+            bool resultado = int.TryParse(EntryCantidad.Text, out cantidad);
+
+            if (resultado)
+            {
+                for (int iterador = 0; iterador < cantidad; iterador++)
+                {
+                    Label label = new Label()
+                    {
+                        Text = iterador.ToString()
+                    };
+                    StackLayoutListadoNumeros.Children.Add(label);
+                }
+            }
+            else
+            {
+                DisplayAlert("Error", "El texto ingresado no es un numero", "OK");
+            }
+        }
+    }
+}
